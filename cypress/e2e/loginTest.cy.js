87dc4f4f5a0485d6cfe023018ba635812a4056e7
ptrckmnlo <manalopatricks@gmail.com>
2023-03-28 20:55:34 +0800

    add test script for login

--- cypress/e2e/loginTest.cy.js
+++ cypress/e2e/loginTest.cy.js
@@ -13,18 +13,28 @@ describe('Login Test', () => {
     cy.get('#inventory_container').should('be.visible');
   });
 
-  it('2. Check that user cannot login with incorrect credentials', function () {
-    const { username, password } = this.data.invalidCredentials;
-    cy.login(username, password);
-    cy.get('[data-test="error"]').should('be.visible')
-  });
-
-  it('3. Check that user can be able to logout from Product page', function () {
+  it('2. Check that user can be able to logout from Product page', function () {
     const { username, password } = this.data.stdUser;
     cy.login(username, password);
     cy.get('.bm-burger-button').click();
     cy.get('#logout_sidebar_link').click();
     cy.get('#login_button_container').should('be.visible')
+  });
+
+  it('3. Check that user cannot login with incorrect credentials', function () {
+    const { username, password } = this.data.invalidCredentials;
+    cy.login(username, password);
+    cy.get('[data-test="error"]').should('contain', 'Epic sadface: Username and password do not match any user in this service')
+  });
+
+  it('4. Check that each error appears upon login with incomplete credentials', function () {
+    cy.get('[data-test="username"]').as('user').type('demo');
+    cy.get('[data-test="login-button"]').as('loginBtn').click();
+    cy.get('[data-test="error"]').as('errorMsg').should('contain', 'Epic sadface: Password is required')
+    cy.get('@user').clear()
+    cy.get('[data-test="password"]').type('demo')
+    cy.get('@loginBtn').click()
+    cy.get('@errorMsg').should('contain', 'Epic sadface: Username is required')
   });
 
 })
